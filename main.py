6f277d7e70502d3d79dc40a3dae7e4545af33150
Ilyas Boudhaine <ilyasboudhaine1@gmail.com>
2024-01-29 20:02:32 +0100

    croped hand frame

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from cvzone.HandTrackingModule import HandDetector
 
 cap  = cv2.VideoCapture(0)
 
-detector = HandDetector(staticMode=False, maxHands=2, modelComplexity=1, detectionCon=0.5, minTrackCon=0.5)
+detector = HandDetector(staticMode=False, maxHands=1, modelComplexity=1, detectionCon=0.5, minTrackCon=0.5)
+
+offset = 20
 
 while True:
     ret, frame = cap.read()
@@ -11,6 +13,12 @@ while True:
     hands, frame = detector.findHands(frame)
     
     cv2.imshow('Hand Detector', frame)
+
+    if hands:
+        hand = hands[0]
+        x, y, w, h = hand["bbox"]
+        handFrame = frame[y - offset:y + h + offset, x - offset:x + w + offset]
+        cv2.imshow('Hand Frame', handFrame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
